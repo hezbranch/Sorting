2d5185d51f19b0c9bf665e9720b6b816d2475df0
Hezekiah Branch <hezekiah.branch@tufts.edu>
2022-01-22 21:01:23 -0500

    Adding timing feature

--- InsertionSort.py
+++ InsertionSort.py
@@ -4,20 +4,32 @@
 ## Purpose: O(n^2) insertion sort
 ## to be ran on the command line
 
+# Import time module
+# Follows PEP-8 Style
+import time
+
 class InsertionSort:
+    
+    # Object initialization
     def __init__(self, input):
         self.data = input
-
+        self.duration = None
+        
+    # Custom define print function
     def __str__(self):
         return "{}".format(self.data)
-
+        
+    # In-place swap operation
     def swap(self, i, j):
         temp = self.data[i]
         self.data[i] = self.data[j]
         self.data[j] = temp
         print(self.data)
 
+    # Insertion Sort implementation
     def sort(self):
+        # Begin timing sort runtime
+        start = time.time()
         for j in range(1, len(self.data)):
             key = self.data[j]
             # Insert key into sorted sequence
@@ -27,4 +39,21 @@ class InsertionSort:
                 self.data[i + 1] = self.data[i]
                 i = i - 1
             self.data[i + 1] = key
+        # Finish timing
+        end = time.time()
+        self.duration = end - start
+        # Display output to user
         print(self.data)
+        
+    # Provide runtime of Insertion Sort
+    def time_it(self):
+        # If time available already generated
+        if self.duration:
+            print(format(self.duration, '.9f'), "seconds")
+            print("Access duration value using .duration")
+        else:
+            # Generate time data
+            self.sort()
+            print(format(self.duration, '.9f'), "seconds")
+            print("Access duration value using .duration")
+
